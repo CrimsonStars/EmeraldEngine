f05577279eaecef6cdfd3e6fe2621f9cadef28d2
CrimsonStars <34871858+CrimsonStars@users.noreply.github.com>
2024-09-26 18:25:53 +0200

    Beta vector 2D added and sample mouse input class.

---
+++ InputTesting.ConsoleApp/Model/Additional structures/Vector2d.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace InputTesting.ConsoleApp.Model.Additional_structures
+{
+    public class Vector2d
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2d Normalise()
+        {
+            var result = new Vector2d();
+
+
+
+            return result;
+        }
+
+        #region Public constructors
+        public Vector2d(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector2d() => new Vector2d(0, 0);
+        #endregion
+    }
+}
--- InputTesting.ConsoleApp/Model/MouseInput.cs
+++ InputTesting.ConsoleApp/Model/MouseInput.cs
@@ -3,12 +3,11 @@
     public sealed class MouseInput
     {
         #region Fields & properties
-
         private MouseInput? _instance;
         private static bool LMB { get; set; }
         private static bool RMB { get; set; }
         private static bool MMB { get; set; }
-
+        private static object Coordinates { get; set; }
         #endregion Fields & properties
 
         public MouseInput GetInstance()
