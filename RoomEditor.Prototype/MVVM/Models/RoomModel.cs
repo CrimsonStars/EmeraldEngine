4c8b709008c80aa9ec2974e46e4c53ede1bd9012
CrimsonStars <34871858+CrimsonStars@users.noreply.github.com>
2024-10-03 15:48:46 +0200

    First elements added

---
+++ RoomEditor.Prototype/MVVM/Models/RoomModel.cs
@@ -0,0 +1,21 @@
+﻿namespace RoomEditor.Prototype.MVVM.Models
+{
+    using EmeraldEngine.Models;
+    using System.Collections.Generic;
+
+    internal class RoomModel
+    {
+        private List<Room> _roomsCollection;
+
+        public RoomModel()
+        {
+            _roomsCollection = new List<Room>();
+        }
+
+        public void AddRoom(Room room)
+        {
+            _roomsCollection.Add(room);
+            
+        }
+    }
+}
