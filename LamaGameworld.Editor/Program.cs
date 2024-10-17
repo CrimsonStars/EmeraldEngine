e7d8800e24982dde642df540ac2a82a338b282e4
CrimsonStars <34871858+CrimsonStars@users.noreply.github.com>
2024-10-17 04:30:03 +0200

    Testing another lib for TUI

--- LamaGameworld.Editor/Program.cs
+++ LamaGameworld.Editor/Program.cs
@@ -1,15 +1,26 @@
 ﻿using LamaGameworld.Editor;
+using LamaGameworld.Editor.Views;
 using Terminal.Gui;
 
 internal class Program
 {
+    private static Window ViewA;
+    private static Window ViewB;
+
+
     private static void Main(string[] args)
     {
         Application.Init();
+        var top = Application.Top;
+        var window = new ComplexWindow("Placeholder text");
 
         try
         {
-            Application.Run(new MyView());
+            top.Add(window);
+            window = new ComplexWindow("Dupa");
+            top.Add(window);
+            Application.Run();
+
         }
         finally
         {
---
+++ TestingLibrary.TUI/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace TestingLibrary.TUI
+{
+    using Spectre.Console;
+    using System;
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            var panelLeft = new Panel("Left");
+            panelLeft.Border=BoxBorder.Rounded;
+            panelLeft.Header = new PanelHeader("Rooms");
+            panelLeft.Expand();
+
+            var panelRight = new Panel("Right");            
+
+            var tempLayout = new Layout("temp");
+            tempLayout.SplitColumns(
+                new Layout("left"),
+                new Layout("right")
+                );
+
+            tempLayout["left"].Ratio = 3;
+            tempLayout["right"].Ratio = 7;
+
+            tempLayout["right"].Update(panelRight);
+            tempLayout["left"].Update(panelLeft);
+
+            AnsiConsole.Write(tempLayout);
+
+            while(true)
+            {
+
+            }
+            
+
+        }
+    }
+}
