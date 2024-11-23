dcc9e43abe57b79935483e0354a0af5e123d9a03
CrimsonFellow92 <sprycha.b@gmail.com>
2024-11-23 02:48:01 +0100

    Fixes in tests + modifications in Gameworld class (accessibility and constructor fix).

--- EmeraldEngine.Tests/GameworldTests.cs
+++ EmeraldEngine.Tests/GameworldTests.cs
@@ -1,47 +1,52 @@
 namespace EmeraldEngine.Tests
 {
-    using EmeraldEngine.Models;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+   using EmeraldEngine.Models;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-    [TestClass]
-    public class GameworldTests
-    {
-        //private static Gameworld? _gameworldEntity; 
+   [TestClass]
+   public class GameworldTests
+   {
+      //private static Gameworld? _gameworldEntity;
 
-        [ClassInitialize]
-        public static void ClassInit(TestContext context)
-        {
-            if (Gameworld.Instance() is null)
-            {
-                throw new Exception("Gameworld instance has encountered some difficulties (null value).");
-            }
+      [ClassInitialize]
+      public static void ClassInit(TestContext context)
+      {
+         if (Gameworld.Instance() is null)
+         {
+            throw new Exception("Gameworld instance has encountered some difficulties (null value).");
+         }
 
-            Gameworld.Instance().ConstructWorld();
-        }
+         Gameworld.Instance().ConstructWorld();
+      }
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.Fail();
-        }
+      [TestMethod]
+      public void Given_GameworldExample_When_ReadingGameworldItems_Then_CheckIfValid()
+      {
+         var gameRooms = Gameworld.Instance()._gameRooms;
 
-        [TestMethod]
-        public void Given_GameworldExample_When_ReadingProperties_Then_CheckCount()
-        {
-            //Assert.IsTrue(Gameworld.Instance());
+         Assert.IsNotNull(gameRooms);
+         Assert.IsTrue(gameRooms.Any());
+         Assert.AreEqual(gameRooms.Count, 7);
+      }
 
-            var numOfRooms = 0;
-            var numOfItems = 0;
+      [TestMethod]
+      public void Given_GameworldExample_When_ReadingGameworldRooms_Then_CheckIfValid()
+      {
+         var gameItems = Gameworld.Instance()._gameItem;
 
-            Assert.IsTrue(numOfItems > 0);
-            Assert.IsTrue(numOfRooms > 0);
-        }
+         Assert.IsNotNull(gameItems);
+         Assert.IsTrue(gameItems.Any());
+         Assert.AreEqual(gameItems.Count, 6);
+      }
 
-        [TestMethod]
-        public void Given_GameworldExample_When_ReadProperties_CheckIfGameworldAndCurrentroomAndPlayerIsNotNull()
-        {
-            Assert.IsNotNull(Gameworld.Instance());
-            Assert.IsNotNull(Gameworld.Instance());
-        }
-    }
+      [TestMethod]
+      public void Given_GameworldExample_When_ReadProperties_CheckIfGameworldAndCurrentroomAndPlayerIsNotNull()
+      {
+         var gameInstance = Gameworld.Instance();
+
+         Assert.IsNotNull(gameInstance);
+         Assert.IsNotNull(gameInstance._player);
+         Assert.IsNotNull(gameInstance._currentRoom);
+      }
+   }
 }
--- EmeraldEngine/Models/Gameworld.cs
+++ EmeraldEngine/Models/Gameworld.cs
@@ -1,222 +1,223 @@
 ﻿namespace EmeraldEngine.Models
 {
-    /// <summary>
-    /// Gameworld singleton which contains:
-    /// <list type="bullet">
-    ///     <item>gameworld instance,</item>
-    ///     <item>Player type of... well <typeparamref name="Player"/>,</item>
-    ///     <item>list of all Rooms in gameworld,</item>
-    ///     <item>list of all items in gameworld,</item>
-    ///     <item>current room where player is property (Room type for now).</item>
-    /// </list>
-    /// </summary>
-    public class Gameworld
-    {
-        #region Properties
+   /// <summary>
+   /// Gameworld singleton which contains:
+   /// <list type="bullet">
+   ///     <item>gameworld instance,</item>
+   ///     <item>Player type of... well <typeparamref name="Player"/>,</item>
+   ///     <item>list of all Rooms in gameworld,</item>
+   ///     <item>list of all items in gameworld,</item>
+   ///     <item>current room where player is property (Room type for now).</item>
+   /// </list>
+   /// </summary>
+   public class Gameworld
+   {
+      #region Properties
 
-        private Player _player;
-        private Room _currentRoom;
-        private Dictionary<string, Item> _gameItem;
-        private Dictionary<string, Room> _gameRooms;
-        private static Gameworld? _instance = null;
+      private static Gameworld? _instance = null;
+      public Room _currentRoom { get; private set; }
+      public Player _player { get; private set; }
+      public Dictionary<string, Item> _gameItem { get; private set; }
+      public Dictionary<string, Room> _gameRooms { get; private set; }
 
-        #endregion Properties
+      #endregion Properties
 
-        #region Constructors and initiators
+      #region Constructors and initiators
 
-        private Gameworld()
-        {
-            _player = new Player();
-            _gameItem = new Dictionary<string, Item>();
-        }
+      private Gameworld()
+      {
+         _player = new Player();
+         _gameItem = new Dictionary<string, Item>();
+         _gameRooms = new Dictionary<string, Room>();
+      }
 
-        public static Gameworld Instance()
-        {
-            if (_instance == null)
-            {
-                _instance = new Gameworld();
-            }
+      public static Gameworld Instance()
+      {
+         if (_instance == null)
+         {
+            _instance = new Gameworld();
+         }
 
-            return _instance;
-        }
+         return _instance;
+      }
 
-        //public static Gameworld MockedGameworldInstance()
-        //{
-        //    if (_instance == null)
-        //    {
-        //        _instance = new Gameworld();
-        //        _instance.ConstructWorld();
-        //    }
+      //public static Gameworld MockedGameworldInstance()
+      //{
+      //    if (_instance == null)
+      //    {
+      //        _instance = new Gameworld();
+      //        _instance.ConstructWorld();
+      //    }
 
-        //    return _instance;
-        //}
+      //    return _instance;
+      //}
 
-        #endregion Constructors and initiators
+      #endregion Constructors and initiators
 
-        public void ConstructWorld()
-        {
-            #region Populate the items dictionary/list
+      public void ConstructWorld()
+      {
+         #region Populate the items dictionary/list
 
-            var id = "ITEM." + Guid.NewGuid();
-            _gameItem[id] = new Item()
-            {
-                Name = "Golden key",
-                ObjectId = id,
-                Description = string.Empty,
-                Value = 0.0f,
-                Weight = 0.0f,
-                Pickable = false,
-            };
+         var id = "ITEM." + Guid.NewGuid();
+         _gameItem[id] = new Item()
+         {
+            Name = "Golden key",
+            ObjectId = id,
+            Description = string.Empty,
+            Value = 0.0f,
+            Weight = 0.0f,
+            Pickable = false,
+         };
 
-            id = "ITEM." + Guid.NewGuid();
-            _gameItem[id] = new Item()
-            {
-                Name = "Wooden hammer",
-                ObjectId = id,
-                Description = "A hammer... a wooden one.",
-                Value = 0.0f,
-                Weight = 0.0f,
-                Pickable = true,
-            };
+         id = "ITEM." + Guid.NewGuid();
+         _gameItem[id] = new Item()
+         {
+            Name = "Wooden hammer",
+            ObjectId = id,
+            Description = "A hammer... a wooden one.",
+            Value = 0.0f,
+            Weight = 0.0f,
+            Pickable = true,
+         };
 
-            id = "ITEM." + Guid.NewGuid();
-            _gameItem[id] = new Item()
-            {
-                Name = "Tarot card [XIII]",
-                ObjectId = id,
-                Description = "Pirogi do nogi i syngal to wrogi. It's a prank item, bra!",
-                Value = 0.0f,
-                Weight = 0.0f,
-                Pickable = false,
-            };
+         id = "ITEM." + Guid.NewGuid();
+         _gameItem[id] = new Item()
+         {
+            Name = "Tarot card [XIII]",
+            ObjectId = id,
+            Description = "Pirogi do nogi i syngal to wrogi. It's a prank item, bra!",
+            Value = 0.0f,
+            Weight = 0.0f,
+            Pickable = false,
+         };
 
-            id = "ITEM." + Guid.NewGuid();
-            _gameItem[id] = new Item()
-            {
-                Name = "Rusty knife",
-                ObjectId = id,
-                Description = "It's not sharp but has a lot of rust on itself.",
-                Value = 0.0f,
-                Weight = 0.0f,
-                Pickable = true,
-            };
+         id = "ITEM." + Guid.NewGuid();
+         _gameItem[id] = new Item()
+         {
+            Name = "Rusty knife",
+            ObjectId = id,
+            Description = "It's not sharp but has a lot of rust on itself.",
+            Value = 0.0f,
+            Weight = 0.0f,
+            Pickable = true,
+         };
 
-            id = "ITEM." + Guid.NewGuid();
-            _gameItem[id] = new Item()
-            {
-                Name = "Potato",
-                ObjectId = id,
-                Description = "What can I say? A potato.",
-                Value = 0.0f,
-                Weight = 0.0f,
-                Pickable = true,
-            };
+         id = "ITEM." + Guid.NewGuid();
+         _gameItem[id] = new Item()
+         {
+            Name = "Potato",
+            ObjectId = id,
+            Description = "What can I say? A potato.",
+            Value = 0.0f,
+            Weight = 0.0f,
+            Pickable = true,
+         };
 
-            id = "ITEM." + Guid.NewGuid();
-            _gameItem[id] = new Item()
-            {
-                Name = "Wheat stone",
-                ObjectId = id,
-                Description = "Massive and heavy stone perfect for getting the 'edge'.",
-                Value = 0.0f,
-                Weight = 0.0f,
-                Pickable = false,
-            };
+         id = "ITEM." + Guid.NewGuid();
+         _gameItem[id] = new Item()
+         {
+            Name = "Wheat stone",
+            ObjectId = id,
+            Description = "Massive and heavy stone perfect for getting the 'edge'.",
+            Value = 0.0f,
+            Weight = 0.0f,
+            Pickable = false,
+         };
 
-            #endregion Populate the items dictionary/list
+         #endregion Populate the items dictionary/list
 
-            #region The player is being created here
+         #region The player is being created here
 
-            _player.Name = "Sample player for tests";
-            _player.ObjectId = "PLAYER_0";
+         _player.Name = "Sample player for tests";
+         _player.ObjectId = "PLAYER_0";
 
-            #endregion The player is being created here
+         #endregion The player is being created here
 
-            #region Construct some rooms
+         #region Construct some rooms
 
-            var roomId = "ROOM." + Guid.NewGuid();
-            _gameRooms[roomId] = new Room()
-            {
-                ObjectId = roomId,
-                Name = "ROOM_A",
-                Description = "",
-                DirectionsToGo = {
+         var roomId = "ROOM." + Guid.NewGuid();
+         _gameRooms[roomId] = new Room()
+         {
+            ObjectId = roomId,
+            Name = "ROOM_A",
+            Description = "",
+            DirectionsToGo = {
                     ("north", "ROOM_B",false)
                 }
-            };
+         };
 
-            roomId = "ROOM." + Guid.NewGuid();
-            _gameRooms[roomId] = new Room()
-            {
-                ObjectId = roomId,
-                Name = "ROOM_B",
-                Description = "",
-                DirectionsToGo = {
+         roomId = "ROOM." + Guid.NewGuid();
+         _gameRooms[roomId] = new Room()
+         {
+            ObjectId = roomId,
+            Name = "ROOM_B",
+            Description = "",
+            DirectionsToGo = {
                     ("north", "ROOM_D",true),
                     ("wooden door", "ROOM_C",true),
                     ("south", "ROOM_A",true)
                 }
-            };
+         };
 
-            roomId = "ROOM." + Guid.NewGuid();
-            _gameRooms[roomId] = new Room()
-            {
-                ObjectId = roomId,
-                Name = "ROOM_C",
-                Description = "",
-                DirectionsToGo = {
+         roomId = "ROOM." + Guid.NewGuid();
+         _gameRooms[roomId] = new Room()
+         {
+            ObjectId = roomId,
+            Name = "ROOM_C",
+            Description = "",
+            DirectionsToGo = {
                     ("east", "ROOM_E",true),
                     ("north", "ROOM_G",true),
                     ("wooden door", "ROOM_B",true)
                 }
-            };
+         };
 
-            roomId = "ROOM." + Guid.NewGuid();
-            _gameRooms[roomId] = new Room()
-            {
-                ObjectId = roomId,
-                Name = "ROOM_D",
-                Description = "",
-                DirectionsToGo = {
+         roomId = "ROOM." + Guid.NewGuid();
+         _gameRooms[roomId] = new Room()
+         {
+            ObjectId = roomId,
+            Name = "ROOM_D",
+            Description = "",
+            DirectionsToGo = {
                     ("south", "ROOM_B",true),
                     ("north", "ROOM_F",true)
                 }
-            };
+         };
 
-            roomId = "ROOM." + Guid.NewGuid();
-            _gameRooms[roomId] = new Room()
-            {
-                ObjectId = roomId,
-                Name = "ROOM_E",
-                Description = "",
-                DirectionsToGo = {
+         roomId = "ROOM." + Guid.NewGuid();
+         _gameRooms[roomId] = new Room()
+         {
+            ObjectId = roomId,
+            Name = "ROOM_E",
+            Description = "",
+            DirectionsToGo = {
                     ("west", "ROOM_C",true)
                 }
-            };
+         };
 
-            roomId = "ROOM." + Guid.NewGuid();
-            _gameRooms[roomId] = new Room()
-            {
-                ObjectId = roomId,
-                Name = "ROOM_F",
-                Description = "",
-                DirectionsToGo = {
+         roomId = "ROOM." + Guid.NewGuid();
+         _gameRooms[roomId] = new Room()
+         {
+            ObjectId = roomId,
+            Name = "ROOM_F",
+            Description = "",
+            DirectionsToGo = {
                     ("south", "ROOM_D",true)
                 }
-            };
+         };
 
-            roomId = "ROOM." + Guid.NewGuid();
-            _gameRooms[roomId] = new Room()
-            {
-                ObjectId = roomId,
-                Name = "ROOM_G",
-                Description = "",
-                DirectionsToGo = {
+         roomId = "ROOM." + Guid.NewGuid();
+         _gameRooms[roomId] = new Room()
+         {
+            ObjectId = roomId,
+            Name = "ROOM_G",
+            Description = "",
+            DirectionsToGo = {
                     ("south", "ROOM_C",false)
                 }
-            };
+         };
 
-            #endregion Construct some rooms
-        }
-    }
+         #endregion Construct some rooms
+      }
+   }
 }
