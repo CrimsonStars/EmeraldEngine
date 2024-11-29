ef9757d43cbf56e7d2ab963e3397acbee926c136
CrimsonStars <34871858+CrimsonStars@users.noreply.github.com>
2024-11-29 16:38:37 +0100

    Adding initial GraphTree and Node class with basic test secenarios.

---
+++ EmeraldEngine/Universal/Graphs/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace EmeraldEngine.Universal.Graphs
+{
+    using System.Collections.Generic;
+
+    public class Node<T>
+    {
+        #region Properies
+        public T? NodeValue { get; set; }
+        public HashSet<Node<T>> Children { get; set; }
+        #endregion
+
+        public Node()
+        {
+            Children = new HashSet<Node<T>>();
+        }
+
+        public Node(T ElementValue) : this()
+        {
+            NodeValue = ElementValue;
+        }
+
+
+    }
+}
