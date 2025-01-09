a5bde693c3e4ef26d70aa9bb2752647ff4c66c14
CrimsonStars <34871858+CrimsonStars@users.noreply.github.com>
2025-01-09 17:33:58 +0100

    0015: Add new interface and their draft for implementation.

--- EmeraldEngine/Universal/BaseInformation.cs
+++ EmeraldEngine/Universal/BaseInformation.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmeraldEngine.Universal
 {
-    public abstract class BaseInformation : ExperimentalBaseObject
+    public abstract class BaseInformation : ExperimentalBaseObject, IBasicInformation
     {
         public string Name { get; set; }
         public string ObjectId { get; set; }
@@ -15,6 +15,21 @@
         {
             Name = name;
             ObjectId = objectId;
+        }
+
+        public void SetName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDescription(string desc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetObjectId(string id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ EmeraldEngine/Universal/IBasicInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace EmeraldEngine.Universal
+{
+    internal interface IBasicInformation
+    {
+        public void SetName(string name);
+
+        public void SetDescription(string desc);
+
+        public void SetObjectId(string id);
+    }
+}
